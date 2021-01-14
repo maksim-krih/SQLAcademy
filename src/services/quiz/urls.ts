5d25443c86c8ef874a78ab9a981e725766c72bf7
Maksym Krykhovetskyi <maksym.krykhovetskyi@inoxoft.com>
2021-01-14 02:39:02 +0200

    add passing

--- src/services/quiz/urls.ts
+++ src/services/quiz/urls.ts
@@ -1,2 +1,5 @@
 export const save = "/quizzes";
 export const getAll = "/quizzes";
+export const getById = (id: string) => `/quizzes/${id}`;
+export const runQuery = "/queries";
+export const completeQuiz = "/results";
